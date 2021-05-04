fe3458069f10b1f35e18f06468d4ede06c11431a
c-bool <next199701@gmail.com>
2021-05-04 16:01:44 +0200

    added docstrings

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-import numpy as np
+# Import built-ins
 import logging
+# Import additional libraries
+import numpy as np
 import cv2
 
 
-def remove_zero_pixels(image_filename):
-    if image_filename.lower().endswith('.png'):
-        picture_px = cv2.imread(image_filename, cv2.IMREAD_UNCHANGED)
+def remove_white_pixels(image_filename):
+    """
+    Removing all white pixels ([255, 255, 255]) in image given by filename.
+    This pre-processing is needed because encoded message will be hidden in white pixels.
 
-        try:
-            _ = picture_px.shape  # will throw exception for corrupted/non-image files
-        except Exception as e:
-            print(e)
-            logging.error("Image is not available or corrupted.")
-            exit(1)
-
-        picture_px[np.where((picture_px == [255, 255, 255]).all(axis=2))] = [254, 254, 254]
-
-        return picture_px
-
-    else:
+    :param image_filename: Filename of image to remove all white pixels in as str
+    :return: Image's pixels as numpy array
+    """
+    if not image_filename.lower().endswith('.png'):
         logging.error("Image should be in png format.")
         exit(1)
 
+    picture_px = cv2.imread(image_filename, cv2.IMREAD_UNCHANGED)
+
+    try:
+        _ = picture_px.shape  # will throw exception for corrupted/non-image files
+    except Exception as e:
+        print(e)
+        logging.error("Image is not available or corrupted.")
+        exit(1)
+
+    picture_px[np.where((picture_px == [255, 255, 255]).all(axis=2))] = [254, 254, 254]
+
+    return picture_px
+
 
 def encode(image_filename, msg_to_encode):
-    picture_px = remove_zero_pixels(image_filename)
+    """
+    Calling function for pre-processing input image, then encoding given message in that image.
+
+    :param image_filename: Filename of image to encode message in as str
+    :param msg_to_encode: Message to encode in image as str
+    """
+    picture_px = remove_white_pixels(image_filename)
     rows_for_chars = np.linspace(0, picture_px.shape[0], len(msg_to_encode), endpoint=False, dtype=int)
     for i, char in enumerate(msg_to_encode):
         picture_px[rows_for_chars[i], ord(char)] = [255, 255, 255]
@@ -32,6 +46,12 @@ def encode(image_filename, msg_to_encode):
 
 
 def decode(image_filename):
+    """
+    Decoding message hidden in given image.
+
+    :param image_filename: Filename of image to decode message from as str
+    :return: Decoded message from image as str
+    """
     picture_px = cv2.imread(image_filename, cv2.IMREAD_UNCHANGED)
     encoded_chars_found = np.where((picture_px == [255, 255, 255]).all(axis=2))[1]
 
