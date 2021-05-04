2fa7419b8fd129af88bfb26caecaf75c52584f5d
c-bool <next199701@gmail.com>
2021-05-04 12:00:35 +0200

    changed back to encoding in white pixels

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 
 def remove_zero_pixels(image_filename):
     picture_px = cv2.imread(image_filename, cv2.IMREAD_UNCHANGED)
-    picture_px[np.where((picture_px == [0, 0, 0]).all(axis=2))] = [1, 1, 1]
+    picture_px[np.where((picture_px == [255, 255, 255]).all(axis=2))] = [254, 254, 254]
 
     return picture_px
 
@@ -13,13 +13,13 @@ def encode(image_filename, msg_to_encode):
     picture_px = remove_zero_pixels(image_filename)
     rows_for_chars = np.linspace(0, picture_px.shape[0], len(msg_to_encode), endpoint=False, dtype=int)
     for i, char in enumerate(msg_to_encode):
-        picture_px[rows_for_chars[i], ord(char)] = [0, 0, 0]
+        picture_px[rows_for_chars[i], ord(char)] = [255, 255, 255]
     cv2.imwrite(image_filename, picture_px, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
 
 
 def decode(image_filename):
     picture_px = cv2.imread(image_filename, cv2.IMREAD_UNCHANGED)
-    encoded_chars_found = np.where((picture_px == [0, 0, 0]).all(axis=2))[1]
+    encoded_chars_found = np.where((picture_px == [255, 255, 255]).all(axis=2))[1]
 
     return "".join([chr(x) for x in encoded_chars_found])
 
