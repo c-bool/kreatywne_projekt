7e8eb104f98e7a6602aa5e4ff54e03b596fcd916
jbknes <daniel@pegiel.pl>
2021-05-07 16:38:08 +0200

    Added GUI

--- color_code_main.py
+++ color_code_main.py
@@ -6,6 +6,7 @@ import math
 from cryptography.fernet import Fernet
 import numpy as np
 import cv2
+import easygui
 # Import project files
 import face_recognize as fr
 
@@ -65,7 +66,7 @@ def read_png_image(image_filename):
     return picture_px
 
 
-def encode(image_filename, shift, msg_to_encode):
+def encode(image_filename, shift, msg_to_encode, save_filename):
     """
     Calls function for reading input image, then encoding given message in that image.
 
@@ -94,7 +95,7 @@ def encode(image_filename, shift, msg_to_encode):
         del msg_tab[:3]
 
     logging.info(f"Saving new image with encoded message to encoded_{image_filename}...")
-    cv2.imwrite(f"encoded_{image_filename}", picture_px, [int(cv2.IMWRITE_PNG_COMPRESSION), 4])
+    cv2.imwrite(save_filename, picture_px, [int(cv2.IMWRITE_PNG_COMPRESSION), 4])
 
 
 def decode(image_filename, shift):
@@ -125,5 +126,14 @@ if __name__ == "__main__":
 
     # fr.recognize_face()
     decrypt_access_phrase()
-    encode("ori_photo.png", 0, "Sex reached suppose our whether. Oh really by an manner sister so. One sportsman tolerably him extensive put she immediate. He abroad of cannot looked in . Continuing interested ten stimulated prosperous frequently all boisterous nay. Of oh really he extent horses wicket.She who arrival end how fertile enabled. Brother she add yet see minuter natural smiling article painted. Themselves at dispatched interested insensible am be prosperous reasonably it. In either so spring wished. Melancholy way she boisterous use friendship she dissimilar considered expression. Sex quick arose mrs lived. Mr things do plenty others an vanity myself waited to. Always parish tastes at as mr father dining at. Name were we at hope. Remainder household direction zealously the unwilling bed sex. Lose and gay ham sake met that. Stood her place one ten spoke yet. Head case knew ever set why over. Marianne returned of peculiar replying in moderate. Roused get enable garret estate old county. Entreaties you devonshire law dissimilar terminated.Passage its ten led hearted removal cordial. Preference any astonished unreserved mrs. Prosperous understood middletons in conviction an uncommonly do. Supposing so be resolving breakfast am or perfectly. Is drew am hill from mr. Valley by oh twenty direct me so. Departure defective arranging rapturous did believing him all had supported. Family months lasted simple set nature vulgar him. Picture for attempt joy excited ten carried manners talking how. Suspicion neglected he resolving agreement perceived at an. Her old collecting she considered discovered. So at parties he warrant oh staying. Square new horses and put better end. Sincerity collected happiness do is contented. Sigh ever way now many. Alteration you any nor unsatiable diminution reasonable companions shy partiality. Leaf by left deal mile oh if easy. Added woman first get led joy not early jokes.To shewing another demands to. Marianne property cheerful informed at striking at. Clothes parlors however by cottage on. In views it or meant drift to. Be concern parlors settled or do shyness address. Remainder northward performed out for moonlight. Yet late add name was rent park from rich. He always do do former he highly.e share of first to worse. Weddings and any opinions suitable smallest nay. My he houses or months settle remove ladies appear. Engrossed suffering supposing he recommend do eagerness. Commanded no of depending extremity recommend attention tolerably. Bringing him smallest met few now returned surprise learning jennings. Objection delivered eagerness he exquisite at do in . Warmly up he nearer mr merely me.Made last it seen went no just when of by. Occasional entreaties comparison me difficulty so themselves. At brother inquiry of offices without do my service. As particular to companions at sentiments. Weather however luckily enquire so certain do. Aware did stood was day under ask. Dearest affixed enquire on explain opinion he. Reached who the mrs joy offices pleased. Towards did colonel article any parties.So delightful up dissimilar by unreserved it connection frequently. Do an high room so in paid. Up on cousin ye dinner should in . Sex stood tried walls manor truth shy and three his. Their to years so child truth. Honoured peculiar families sensible up likewise by on in .It as announcing it me stimulated frequently continuing. Least their she you now above going stand forth. He pretty future afraid should genius spirit on. Set property addition building put likewise get. Of will at sell well at as. Too want but tall nay like old. Removing yourself be in answered he. Consider occasion get improved him she eat. Letter by lively oh denote an.")
-    print(decode("encoded_ori_photo.png", 0))
+    while True:
+        if easygui.indexbox(msg='What do you want to do?', title='Color Code', choices=('Encode', 'Decode'), cancel_choice='No'):
+            filename = easygui.fileopenbox(msg="Give image to decode", title=None, default="encoded_ori_photo.png", filetypes="*.png", multiple=False)
+            easygui.msgbox(msg=decode(filename, 0), title='Color Code', ok_button='Exit', root=None)
+        else:
+            msg = easygui.enterbox(msg='Give message to encode', title='Color Code', default='Hello World!', strip=False, root=None)
+            filename = easygui.fileopenbox(msg="Give image to encode", title=None, default="ori_photo.png", filetypes="*.png", multiple=False)
+            save_filename = easygui.filesavebox(msg="Save encoded image as..", title=None, default='encoded_ori_photo.png', filetypes="*.png")
+            encode(filename, 0, msg, save_filename)
+        if easygui.ynbox(msg='Do you want to exit?', title='Color Code', choices=('Yes', 'No')):
+            exit(1)
